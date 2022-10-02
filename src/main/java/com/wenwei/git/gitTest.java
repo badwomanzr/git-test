9bcaacc3773fa6d2e4e2dd9c4801da3c8ccfe4ac
=Wei <=Wei@123.com>
2022-10-02 15:15:38 +0800

    first commit

---
+++ src/main/java/com/wenwei/git/gitTest.java
@@ -0,0 +1,18 @@
+package com.wenwei.git;
+
+/**
+ * @author HP
+ * @ClassName gitTest
+ * @Description TODO
+ * @Date 2022/10/2 15:11
+ * Version 1.0
+ */
+
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
